75b3475eaa4116a47fe6a238b9659d0c83e8975c
mohamed abdi <90768460+moha1633@users.noreply.github.com>
2025-06-10 02:02:00 +0200

    Add files via upload
    Slutlig version av ContosoPizza-API med GET, PUT och struktur för vidare utveckling

---
+++ contosoPizza/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Byt port här:
+builder.WebHost.UseUrls("http://localhost:5050");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
